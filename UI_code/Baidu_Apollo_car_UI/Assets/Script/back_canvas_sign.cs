5a9feba027ef5fd8de0ceba333f2d60edb0b70e1
haoyuhuang <haoyuhuang@bjtu.edu.cn>
2024-02-07 14:15:55 +0800

    first commit

---
+++ UI_code/Baidu_Apollo_car_UI/Assets/Script/back_canvas_sign.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_canvas_sign : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Back_to_sign_in()
+    {
+        SceneManager.LoadScene("UICanvas");
+    }
+}
